7bd509e55a86a1d4d92553bc7bf7710d3d43fd66
Marcella2405 <marcella135@gmail.com>
2016-10-16 14:40:16 -0200

    Ajustes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,5 @@
-var tabs = new ch.Tabs(ch(".demo-tabs")[0]);
-
 var carousel = new ch.Carousel(ch('.demo-carousel')[0], {
     pagination: false
 });
 
 var zoom1 = new ch.Zoom(ch('#zoom-default')[0]);
-
