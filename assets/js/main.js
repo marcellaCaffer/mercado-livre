6ec46bcfd0c962e53976c8622dd9274f03738b01
Marcella2405 <marcella135@gmail.com>
2016-10-15 21:57:48 -0300

    Criando aplicacao

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+var tabs = new ch.Tabs(ch(".demo-tabs")[0]);
+
+var carousel = new ch.Carousel(ch('.demo-carousel')[0], {
+    pagination: false
+});
+
+var zoom1 = new ch.Zoom(ch('#zoom-default')[0]);
---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+var gulp = require('gulp');
+var htmlmin = require('gulp-htmlmin');
+var cleanCSS = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var imagemin = require('gulp-imagemin');
+var less = require('gulp-less');
+var autoprefixer = require('gulp-autoprefixer');
+var watch = require('gulp-watch');
+
+gulp.task('minify', function() {
+  return gulp.src('assets/*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('minify-css', function() {
+  return gulp.src('assets/less/*.less')
+  	.pipe(less())
+  	.pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+    }))
+  	.pipe(concat('style.min.css'))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task( 'compress', function () {
+	return gulp.src('assets/js/*.js')
+	.pipe(concat('script.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task( 'minify-image', function () {
+	return gulp.src('assets/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images'))
+});
+
+gulp.task( 'minify-image', function () {
+	return gulp.src('assets/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images'))
+});
+
+gulp.task('watch', function() {
+ gulp.watch('assets/less/*.less', ['minify-css']);
+ gulp.watch('assets/js/*.js', ['compress']);
+ gulp.watch('assets/images/*', ['minify-image']);
+ gulp.watch('assets/*.html', ['minify']);
+});
+
+
